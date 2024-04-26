4f1070945cb4a6c2407f41e5a289b9807df4575b
IanPoe03 <158785787+IanPoe03@users.noreply.github.com>
2024-04-26 02:57:22 -0400

    Update transcriber_st.py

--- transcriber_st.py
+++ transcriber_st.py
@@ -61,6 +61,7 @@ with st.sidebar:
                 # Show a message while processing
                 with st.spinner('Transcribing audio...'):
                     text = transcribe_audio(uploaded_file)
+                    st.write('transcribed')
                     vectordb = ingest(text)
                     llm = ChatOpenAI(temperature=0.7, model_name="gpt-4-turbo", verbose=False, openai_api_key = OPENAI_API_KEY)
                     embeddings = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY)
